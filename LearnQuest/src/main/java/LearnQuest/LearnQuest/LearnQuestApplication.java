9514020fd575e421659261a999977da159845cec
parker0509 <167055018+parker0509@users.noreply.github.com>
2024-10-04 15:22:20 +0900

    study

---
+++ LearnQuest/src/main/java/LearnQuest/LearnQuest/LearnQuestApplication.java
@@ -0,0 +1,13 @@
+package LearnQuest.LearnQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnQuestApplication.class, args);
+	}
+
+}
---
+++ LearnQuest/src/test/java/LearnQuest/LearnQuest/LearnQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package LearnQuest.LearnQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
